85fa8fc067c9eacff9611a53cf3a139609c5ce7e
Rubèn <numbworks@gmail.com>
2025-05-19 21:13:04 +0000

    #1 MessageCollectionTestCase: added.

--- tests/nwcommitaveragestests.py
+++ tests/nwcommitaveragestests.py
@@ -3,15 +3,68 @@ import unittest
 from unittest.mock import Mock, mock_open, patch
 from argparse import ArgumentParser, Namespace
 from parameterized import parameterized
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional, Tuple, cast
 
 # LOCAL MODULES
 import sys, os
 sys.path.append(os.path.dirname(__file__).replace('tests', 'src'))
-from nwcommitaverages import _MessageCollection, APFactory, APAdapter
+from nwcommitaverages import LOGTYPE, _MessageCollection, APFactory, APAdapter
 
 # SUPPORT METHODS
 # TEST CLASSES
+class MessageCollectionTestCase(unittest.TestCase):
+
+    def test_notenoughdata_shouldreturnexpectedmessage_wheninvoked(self):
+
+        # Arrange
+        expected : str = "Not enough data"
+
+        # Act
+        actual : str = _MessageCollection.not_enough_data()
+
+        # Assert
+        self.assertEqual(expected, actual)
+    def test_providedlogtypenotsupported_shouldreturnexpectedmessage_wheninvalidlogtypegiven(self):
+
+        # Arrange
+        log_type : LOGTYPE = LOGTYPE.DAILY                                      # We fake this is not supported.
+        expected : str = "The provided 'log_type' is not supported ('daily')."
+
+        # Act
+        actual : str = _MessageCollection.provided_log_type_not_supported(log_type)
+
+        # Assert
+        self.assertEqual(expected, actual)
+    def test_parserdescription_shouldreturnexpectedmessage_wheninvoked(self):
+
+        # Arrange
+        expected : str = "Calculates the average commit value and logs the result."
+
+        # Act
+        actual : str = _MessageCollection.parser_description()
+
+        # Assert
+        self.assertEqual(expected, actual)
+    def test_parserfilepath_shouldreturnexpectedmessage_wheninvoked(self):
+
+        # Arrange
+        expected : str = "The file path to the Git repository for which the average commit value is calculated."
+
+        # Act
+        actual : str = _MessageCollection.parser_file_path()
+
+        # Assert
+        self.assertEqual(expected, actual)
+    def test_parserlogtype_shouldreturnexpectedmessage_wheninvoked(self):
+
+        # Arrange
+        expected : str = "The type of log ('table' for a tabular overview, 'daily' and 'monthly' for a list of statuses). The default is 'table'."
+
+        # Act
+        actual : str = _MessageCollection.parser_logtype()
+
+        # Assert
+        self.assertEqual(expected, actual)
 class APFactoryTestCase(unittest.TestCase):
 
     def test_create_shouldreturnexpectedargumentparser_wheninvoked(self) -> None:
