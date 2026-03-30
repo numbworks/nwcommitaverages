bbb660e7de094b553bcbd6feb98baf7f8d4df02a
Rubèn <numbworks@gmail.com>
2026-03-30 18:14:44 +0000

    #2 AsciiBannerManagerTestCase: added (100%).

--- tests/nwcommitaveragestests.py
+++ tests/nwcommitaveragestests.py
@@ -11,7 +11,7 @@ from unittest.mock import Mock, patch
 # LOCAL MODULES
 import sys, os
 sys.path.append(os.path.dirname(__file__).replace('tests', 'src'))
-from nwcommitaverages import LOGTYPE, _MessageCollection, APFactory, APAdapter, CLIManager, CommitAverageCalculator, CommitItem, MonthlyStatus, Summary
+from nwcommitaverages import LOGTYPE, _MessageCollection, APFactory, APAdapter, AsciiBannerManager, CLIManager, CommitAverageCalculator, CommitItem, MonthlyStatus, Summary
 from nwcommitaverages import DailyStatus
 
 # SUPPORT METHODS
@@ -539,6 +539,76 @@ class CommitAverageCalculatorTestCase(unittest.TestCase):
             # Assert
             mocked_orchestrate_logging.assert_called_once_with(summary = summary, log_type = log_type)
             self.assertEqual(expected, actual)
+class AsciiBannerManagerTestCase(unittest.TestCase):
+
+    def test_validate_shouldraisevalueerror_whenversionisnone(self) -> None:
+
+        # Arrange
+        # Act, Assert
+        with self.assertRaises(ValueError) as context:
+            AsciiBannerManager()._AsciiBannerManager__validate(version = None) # type: ignore
+
+        self.assertEqual(_MessageCollection.provided_version_empty_whitespace(), str(context.exception))
+    def test_validate_shouldraisevalueerror_whenversioniswhitespace(self) -> None:
+
+        # Arrange
+        version : str = " "
+
+        # Act, Assert
+        with self.assertRaises(ValueError) as context:
+            AsciiBannerManager()._AsciiBannerManager__validate(version = version) # type: ignore
+
+        self.assertEqual(_MessageCollection.provided_version_empty_whitespace(), str(context.exception))
+    def test_createfiglet_shouldreturnexpectedmaxlength_wheninvoked(self) -> None:
+
+        # Arrange
+        expected : int = 65
+
+        # Act
+        _, max_length = AsciiBannerManager()._AsciiBannerManager__create_figlet() # type: ignore
+
+        # Assert
+        self.assertEqual(expected, max_length)
+    def test_createframe_shouldreturnexpectedtuple_wheninvoked(self) -> None:
+
+        # Arrange
+        version : str = "1.0.5"
+        max_length : int = 65
+        
+        expected_top_line : str = "*" * 65
+        expected_bottom_line : str = "*" * 46 + "Version: 1.0.5" + "*" * 5
+
+        # Act
+        top_line, bottom_line = AsciiBannerManager()._AsciiBannerManager__create_frame(version = version, max_length = max_length) # type: ignore
+
+        # Assert
+        self.assertEqual(expected_top_line, top_line)
+        self.assertEqual(expected_bottom_line, bottom_line)
+    def test_create_shouldcallexpectedprivatemethodsandreturnbanner_wheninvoked(self) -> None:
+
+        # Arrange
+        ascii_banner_manager : AsciiBannerManager = AsciiBannerManager()
+        version : str = "1.0.5"
+        max_lenght : int = 65
+        
+        figlet_tpl : tuple = ("ascii_art", max_lenght)
+        frame_tpl : tuple = ("top_border", "bottom_border")
+
+        with patch.object(ascii_banner_manager, "_AsciiBannerManager__validate") as validate, \
+                patch.object(ascii_banner_manager, "_AsciiBannerManager__create_figlet", return_value = figlet_tpl) as create_figlet, \
+                patch.object(ascii_banner_manager, "_AsciiBannerManager__create_frame", return_value = frame_tpl) as create_frame:
+
+            # Act
+            actual : str = ascii_banner_manager.create(version = version)
+
+            # Assert
+            validate.assert_called_once_with(version)
+            create_figlet.assert_called_once()
+            create_frame.assert_called_once_with(version, max_lenght)
+
+            self.assertIn("top_border", actual)
+            self.assertIn("ascii_art", actual)
+            self.assertIn("bottom_border", actual)
 class CLIManagerTestCase(unittest.TestCase):
 
     @parameterized.expand([
